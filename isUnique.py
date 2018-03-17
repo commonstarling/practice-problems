93a044091d513fc75b9cb3ac06ab50801893a397
Sheena Ramirez <sheenaramirez2@gmail.com>
2018-03-17 13:33:22 -0500

    add python solution for is_unique

--- isUnique.py
+++ isUnique.py
@@ -1,27 +1,42 @@
-# Output - boolean (whether a string has unique characters)
-# Input - string
-# Constraints - none
-# Edge Cases:
-#   string with no length - false
-#   non-string argument - throw an error
+"""
+Output - boolean (whether a string has unique characters)
+Input - string
+Constraints - none
+Edge Cases:
+  string with no length - false
+  non-string argument - throw an error
 
-# Examples
-#   - 'abc' => true
-#   - 'aabc' => false
-#   - '' => false
-#   - ['a', 'b', 'c'] => false
+Examples
+  - 'abc' => true
+  - 'aabc' => false
+  - '' => false
+  - ['a', 'b', 'c'] => false
+"""
 
-def isUnique (str)
+def is_unique(text):
   # If the argument is not a string
+  if not type(text) == str:
     # Throw an error
+    raise ValueError('Argument is not a string')
   # If the argument doesn't have a length
+  if not text:
     # Return false
+    return False
   # If the argument has a length of 1
+  if len(text) == 1:
     # Return true
-  # Create an object that will track letters in the string
+    return True
+  # Create a dictionary that will track letters in the string
+  letter_tracker = {}
   # Loop through the letters in the string
+  for letter in text:
     # If the letter does not exist as a key in the tracking object
+    if not letter in letter_tracker:
       # Set it as a key in the tracking object
+      letter_tracker[letter] = True
     # Else
+    else:
       # Return false
+      return False
   # Return true if we've exited the loop without returning false
+  return True
