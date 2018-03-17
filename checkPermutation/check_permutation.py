153f4518c4c040bfebd1fe586f218d824104199f
Sheena Ramirez <sheenaramirez2@gmail.com>
2018-03-17 16:14:24 -0500

    complete check_permutation

--- checkPermutation/check_permutation.py
+++ checkPermutation/check_permutation.py
@@ -6,6 +6,8 @@ input - two strings
 constraints/complexity - O2(n)
 edge cases -
   if any input is not a string => throw an error
+  different cases => 'L' === 'l'
+  white space => included in our calcuation
 examples -
   'abc' and 'cab' => true
   'car' and 'taxi' => false
@@ -15,11 +17,23 @@ examples -
 """
 
 def check_permuation(str1, str2):
-   # If any input is not a string
-     # Throw an error
-   # If str1's length is not equal to str2's length
-     # Return false
-   # Loop through the letters in str1
-     # If str2 doesn't have the current letter
-       # Return false
-   # Return true if we've exited the loop
+  # If any input is not a string
+  if type(str1) != str or type(str2) != str:
+    # Throw an error
+    raise ValueError('Input is not a string')
+  #If str1's length is not equal to str2's length
+  if len(str1) != len(str2):
+    # Return false
+    return False;
+  # Turn the strings into sorted lists with lowercase letters
+  sorted_str1 = sorted(str1.lower())
+  sorted_str2 = sorted(str2.lower())
+
+  # Loop through the letters in the first list
+  for index, val in enumerate(sorted_str1):
+    # If the current letter does not match the letter in the same position in the second array
+    if sorted_str1[index] != sorted_str2[index]:
+      # Return false
+      return False;
+  # Return true if we've completed the loop
+  return True;
